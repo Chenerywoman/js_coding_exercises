4543a291c5293793dda2cb952c5840ce3adfa8e4
Chenerywoman <rachelmchenery@hotmail.com>
2021-01-29 11:08:39 +0000

    passing meanScores

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -79,7 +79,17 @@ function countLinuxUsers(users) {
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-  // Add your code here!
+  
+  const totalScores = scores.reduce((acc, curr) => {
+      return acc + curr;
+  }, 0);
+
+  let meanScores = totalScores / scores.length;
+
+  if (meanScores % 1 !== 0){
+      meanScores = Number(meanScores.toFixed(2));
+  }
+  return meanScores;
 }
 
 function simpleFizzBuzz(n) {
