358e40f116748d039353e0298e3ba0305be2b79d
Chenerywoman <rachelmchenery@hotmail.com>
2021-01-29 11:36:29 +0000

    formatting amended

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -6,7 +6,7 @@ function capitalize(word) {
 function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  
+
   const firstInitial = firstName.charAt(0).toUpperCase();
   const secondInitial = lastName.charAt(0).toUpperCase();
 
@@ -16,43 +16,43 @@ function generateInitials(firstName, lastName) {
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  
+
   const VAT = vatRate / 100;
   const VATElement = originalPrice * VAT;
   const priceWithVAT = originalPrice + VATElement
-  const priceWithVATRounded  = priceWithVAT.toFixed(2);
+  const priceWithVATRounded = priceWithVAT.toFixed(2);
   return Number(priceWithVATRounded);
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  
-  const amountToDeduct = originalPrice * reduction/100;
-  const reduducedPrice  = originalPrice - amountToDeduct;
+
+  const amountToDeduct = originalPrice * reduction / 100;
+  const reduducedPrice = originalPrice - amountToDeduct;
   const rounded = Number(reduducedPrice.toFixed(2))
   return rounded;
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  
- return str.length % 2 == 0 ? str.charAt(str.length / 2 - 1) + str.charAt(str.length / 2) : str.charAt(str.length / 2);
+
+  return str.length % 2 == 0 ? str.charAt(str.length / 2 - 1) + str.charAt(str.length / 2) : str.charAt(str.length / 2);
 
 }
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
 
-  const reversedArray =  word.split(' ').reverse();
+  const reversedArray = word.split(' ').reverse();
 
   const reversedWordsArray = reversedArray.map((elem) => {
-      return elem.split('').reverse().join('');
+    return elem.split('').reverse().join('');
   });
   const reversed = reversedWordsArray.join(' ');
 
   return reversed;
- 
+
 }
 
 function reverseAllWords(words) {
@@ -61,13 +61,13 @@ function reverseAllWords(words) {
   const reversedArray = words.map((elem) => {
     return elem.split('').reverse().join('');
   });
-  
+
   return reversedArray;
 }
 
 function countLinuxUsers(users) {
   if (users === undefined) throw new Error("users is required");
-  
+
   const linuxUsers = users.reduce((acc, curr) => {
 
     return curr.type === "Linux" ? acc + 1 : acc;
@@ -79,22 +79,22 @@ function countLinuxUsers(users) {
 
 function getMeanScore(scores) {
   if (scores === undefined) throw new Error("scores is required");
-  
+
   const totalScores = scores.reduce((acc, curr) => {
-      return acc + curr;
+    return acc + curr;
   }, 0);
 
   let meanScores = totalScores / scores.length;
 
-  if (meanScores % 1 !== 0){
-      meanScores = Number(meanScores.toFixed(2));
+  if (meanScores % 1 !== 0) {
+    meanScores = Number(meanScores.toFixed(2));
   }
   return meanScores;
 }
 
 function simpleFizzBuzz(n) {
   if (n === undefined) throw new Error("n is required");
-  
+
   if (n % 3 == 0 && n % 5 == 0) return "fizzbuzz";
   else if (n % 3 === 0) return "fizz";
   else if (n % 5 === 0) return "buzz";
--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,24 +1,24 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-    return sandwich.fillings;
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-    return person.city === "Manchester" ? true : false;
+  return person.city === "Manchester" ? true : false;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  
-    const busesNeeded = Math.ceil(people / 40);
 
-    return busesNeeded;
+  const busesNeeded = Math.ceil(people / 40);
+
+  return busesNeeded;
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  
+
   const sheep = arr.reduce((acc, curr) => {
     return curr === "sheep" ? acc + 1 : acc;
   }, 0);
@@ -28,7 +28,7 @@ function countSheep(arr) {
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  
+
   return person.address.postCode.charAt(0) === "M" && person.address.city === "Manchester" ? true : false;
 }
 
