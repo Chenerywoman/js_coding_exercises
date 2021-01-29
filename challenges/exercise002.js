87257cb95941692f7d3bb17af5860d2d7f9c1256
Chenerywoman <rachelmchenery@hotmail.com>
2021-01-29 11:33:21 +0000

    passing all tests

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,35 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+    return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+    return person.city === "Manchester" ? true : false;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  
+    const busesNeeded = Math.ceil(people / 40);
+
+    return busesNeeded;
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  
+  const sheep = arr.reduce((acc, curr) => {
+    return curr === "sheep" ? acc + 1 : acc;
+  }, 0);
+
+  return sheep;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  
+  return person.address.postCode.charAt(0) === "M" && person.address.city === "Manchester" ? true : false;
 }
 
 module.exports = {
